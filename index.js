7e260d5391e273492dbd33cd45c341c8895dbdb7
Railson Ferreira <railson.ferreira@estudante.ifgoiano.edu.br>
2022-11-24 00:40:55 +0000

    Timestamp Microservice

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // index.js
 // where your node app starts
+require('dotenv').config()
 
 // init project
 var express = require('express');
@@ -18,10 +19,27 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
+});
+
+
+
+
+app.get("/api/:date?", function (req, res) {
+  const dateStr = req.params.date;
+  if(!dateStr){
+    const now = new Date();
+    res.json({unix: now.getTime(), utc: now.toUTCString()})
+  }else{
+    const date =  Number(dateStr)?new Date(Number(dateStr)): new Date(dateStr);
+    if(isNaN(date.getTime())){
+      res.json({error: "Invalid Date"})
+    }else{
+      res.json({unix: date.getTime(), utc: date.toUTCString()})
+    }
+  }
 });
 
 
